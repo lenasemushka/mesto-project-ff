2fa8362b27fa527df470cf0a15ceb91c21e8f20a
Elena Semenova <lena.semenova97@mail.ru>
2025-01-16 15:52:54 +0300

    all project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/lenasemushka/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,38 @@
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: Функция создания карточки
+function addCard(card, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+  const cardTitle = cardElement.querySelector('.card__title');
+
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
+  cardTitle.textContent = card.name;
+
+  cardDeleteButton.addEventListener('click', () => deleteCard(cardElement));
+  
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+function showCards(cards) {
+  cards.forEach((cardData) => {
+      const card = addCard(cardData, deleteCard);
+      placesList.appendChild(card);
+  });
+}
+showCards(initialCards);
+
